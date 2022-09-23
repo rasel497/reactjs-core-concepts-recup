3baa088ce33400f8ff55193fb346c8e3bc0418bb
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-23 12:54:07 +0600

    create a bsic Componant and css style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <District></District>
+      <District></District>
 
     </div>
   );
 }
 
+// create a bsic Componant and css style
+function District() {
+  return (
+    <div className='district'>
+      <h1>Name: </h1>
+      <p>Speciality: </p>
+    </div>
+  )
+}
 export default App;
