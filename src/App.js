00ba6660cea475cd9d4d66bbab55fb1457e73cd9
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-23 14:44:46 +0600

    create useState inside the Componant and set button Event listerner increse

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -16,16 +17,27 @@ function App() {
 const districtStyle = {
   backgroundColor: 'yellow',
   margin: '20px',
+  padding: '5px',
   borderRadius: '20px'
 }
 
 // create a bsic Componant and css style
 function District(props) {
+  // create state and destructuring
+  const [power, setPower] = useState(1);
+
+  const bootsPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+
   return (
     // send dynamic data using props in APP show UI
     <div style={districtStyle}>
       <h1>Name: {props.name}</h1>
       <p>Speciality: {props.speciality}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={bootsPower}>Boots The Power</button>
     </div>
   )
 }
